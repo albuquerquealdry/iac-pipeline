d34e10faca04c969607f5582bd7b91055b7d9414
Aldry Albuquerque <albuquerquealdry@gmail.com>
2022-10-31 22:14:43 -0300

    push update

--- variables.tf
+++ variables.tf
@@ -1,45 +1,4 @@
-variable "nome_repositorio" {
-  type    = string
-  default = "cluster-k8s"
-}
-
-variable "cluster_name" {
-  type    = string
-  default = "cluster-k8s"
-}
-
-
-variable "region" {
-  type    = string
-  default = "us-west-2"
-}
-
-variable "group_machine_name" {
-  type        = string
-  description = "Node for project"
-  default     = "Kubernetes Node"
-}
-
-variable "min_size" {
-  type        = number
-  description = "Number min Nodes"
-  default     = 1
-}
-
-variable "max_size" {
-  type        = number
-  description = "Number max Nodes"
-  default     = 10
-}
-
-variable "desired_size" {
-  type        = number
-  description = "Number Persistence Nodes"
-  default     = 3
-}
-
-variable "instance_type" {
-  type        = list(any)
-  description = "Type Machine for nodes"
-  default     = ["t2.micro"]
+variable "ec2_name" {
+  type = string
+  default = "Simbora virar monstro"
 }
